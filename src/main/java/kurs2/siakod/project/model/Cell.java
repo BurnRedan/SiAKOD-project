81e54b0886dae16c05f46c5883f7982faa6f575a
BurnRedan <72151297+burnredan@users.noreply.github.com>
2022-02-17 16:38:04 +0300

    Изменение Cell

--- src/main/java/kurs2/siakod/project/model/Cell.java
+++ src/main/java/kurs2/siakod/project/model/Cell.java
@@ -5,7 +5,22 @@ public class Cell {
     int x, y;
     Unit stayingUnit;
 
-    public Cell(Cell south, Cell west, Cell east, Cell north){
+    public Cell(Cell south, Cell west, Cell east, Cell north, int x, int y){
+        this.south = south;
+        this.west = west;
+        this.east = east;
+        this.north = north;
+        this.x = x;
+        this.y =y;
+    }
 
+    public Cell(Cell south, Cell west, Cell east, Cell north, int x, int y, Unit unit){
+        this.south = south;
+        this.west = west;
+        this.east = east;
+        this.north = north;
+        this.x = x;
+        this.y =y;
+        this.stayingUnit = unit;
     }
 }
