9810165c90a35d41b236c25ff92f90836482055b
BurnRedan <danilaush@gmail.com>
2022-03-10 12:34:42 +0300

    Изменение тестов топологии

--- src/test/java/com/vsu/service/grid/PlaneTopologyTest.java
+++ src/test/java/com/vsu/service/grid/PlaneTopologyTest.java
@@ -15,6 +15,7 @@ class PlaneTopologyTest {
         Grid grid = new Grid();
         GridService gridService = new GridService();
         gridService.initGrid(grid, 4, 4, new PlaneTopology());
+
         assertEquals(grid.getMatrix()[1][1].north(grid), grid.getMatrix()[0][1]);
         assertNull(grid.getMatrix()[0][1].north(grid));
         assertEquals(grid.getMatrix()[3][3].north(grid), grid.getMatrix()[2][3]);
@@ -22,10 +23,24 @@ class PlaneTopologyTest {
 
     @Test
     void south() {
+        Grid grid = new Grid();
+        GridService gridService = new GridService();
+        gridService.initGrid(grid, 4, 4, new PlaneTopology());
+
+        assertEquals(grid.getMatrix()[1][1].south(grid), grid.getMatrix()[2][1]);
+        assertNull(grid.getMatrix()[3][1].south(grid));
+        assertEquals(grid.getMatrix()[2][3].south(grid), grid.getMatrix()[3][3]);
     }
 
     @Test
     void west() {
+        Grid grid = new Grid();
+        GridService gridService = new GridService();
+        gridService.initGrid(grid, 4, 4, new PlaneTopology());
+
+        assertEquals(grid.getMatrix()[1][1].west(grid), grid.getMatrix()[1][0]);
+        assertNull(grid.getMatrix()[0][0].west(grid));
+        assertEquals(grid.getMatrix()[3][2].west(grid), grid.getMatrix()[3][1]);
     }
 
     @Test
@@ -36,7 +51,6 @@ class PlaneTopologyTest {
 
         assertEquals(grid.getMatrix()[1][1].east(grid), grid.getMatrix()[1][2]);
         assertNull(grid.getMatrix()[0][3].east(grid));
-        Tile tmp = grid.getMatrix()[3][2].east(grid);
-        assertEquals(tmp, grid.getMatrix()[3][3]);
+        assertEquals(grid.getMatrix()[3][2].east(grid), grid.getMatrix()[3][3]);
     }
 }
